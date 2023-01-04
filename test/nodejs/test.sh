0ba88c615d7ca8098bcb9394f2b039508ecb09f9
Christian Sutter <git@doublesignal.com>
2023-01-04 17:41:03 +0000

    Add basic Node.js container template

---
+++ test/nodejs/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+source "$(dirname "$0")/../harness.sh"
+
+setup "nodejs" "18.12.1"
+
+run_test "Node version is correct" "node -v" "18.12.1"
+run_test "NVM is present" "npm --help" "npm <command>"
+run_test "Sample script runs" "node sample.js" "Hello world"
+run_test "Container defaults to non-root user" "whoami" "node"
+run_test "Non-root user is able to sudo" "sudo whoami" "root"
